ba9dd7d28efdbb8aa6c98e3e7da6ab7fd6089253
casal <lautarolopez213@gmail.com>
2024-06-17 22:29:04 -0300

    Agregar archivos de proyecto.

---
+++ Reto/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Reto
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Reto/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Reto.Models;
+
+
+namespace Reto
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public List<FruitInformation> PapayaInfo { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+            PapayaInfo = new List<FruitInformation>()
+
+            {
+                new FruitInformation()
+                {
+
+                Micronutrient = "Vitamin C",
+                Percentage = 101
+                },
+
+                 new FruitInformation()
+                {
+
+                Micronutrient = "Vitamin A",
+                Percentage = 19
+                },
+
+                  new FruitInformation()
+                {
+
+                Micronutrient = "Calcium",
+                Percentage = 2
+                },
+
+                   new FruitInformation()
+                {
+
+                Micronutrient = "Magnesium",
+                Percentage = 5
+                },
+
+                    new FruitInformation()
+                {
+
+                Micronutrient = "Potassium",
+                Percentage = 5
+                },
+            };
+            this.BindingContext = this;
+        }
+
+    }
+
+}
